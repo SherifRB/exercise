a213ea1ccfb6825b1715545f4af0f3ffa6e46471
SherifRB <sheruf.b@mail.ru>
2022-03-17 23:56:40 +0900

    GameNumberExtended

---
+++ GameNumber/GameNumberExt/Program.cs
@@ -0,0 +1,49 @@
+﻿int min = 1;
+int max = 100;
+int num = (max - min) / 2;
+string answer; //ответ пользователя
+
+//Рандом и массив со случайными ответами
+Random rnd = new Random();
+string[] answers = new string[10] {"Возможно, это ", "Наверное, это ", "Я знаю! Это ", "Предположим, что это ", "Думаю, это ", "Определенно, ", "Хм.. Может быть, ", "Как насчет ", "Допустим, ", "Наверняка это "};
+
+bool checkAnswer = false; //проверка на правильность ответа
+
+Console.WriteLine("Добро пожаловать в игру! Я угадаю загаданное тобой число");
+Console.WriteLine($"Напиши '+', если мое число больше твоего, '-', если меньше, и '=', если я угадал: {num}?");
+
+while (checkAnswer != true)
+{
+    answer = Console.ReadLine();
+    
+    switch (answer)
+    {
+        case "+":
+            if (num == num - (num - min) / 2) //проверка на максимальное количество делений
+            {
+                Console.WriteLine($"Хм.. Вы не ошиблись? Это не число {num}?");
+                break;
+            }
+            max = num;
+            num = num - (max - min) / 2;
+            Console.WriteLine($"{answers[rnd.Next(answers.Length)]}{num}?");
+            break;
+        case "-":
+            if (num == num + (num - min) / 2)
+            {
+                Console.WriteLine($"Хм.. Вы не ошиблись? Это не число {num}?");
+                break;
+            }
+            min = num;
+            num = num + (max - min) / 2;
+            Console.WriteLine($"{answers[rnd.Next(answers.Length)]}{num}?");
+            break;
+        case "=":
+            checkAnswer = true;
+            Console.WriteLine($"Ура! Я угадал твое число {num}!");
+            break;
+        default:
+            Console.WriteLine("Вы ввели неверный символ, попробуйте еще раз");
+            break;
+    }
+}
